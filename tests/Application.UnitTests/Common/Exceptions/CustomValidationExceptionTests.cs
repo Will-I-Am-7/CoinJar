7543335dcf9af3442500082d05acc5b82e506e63
William Madgwick <WMadgwick@netrec.co.za>
2021-11-07 17:45:37 +0200

    test: added application unit tests

---
+++ tests/Application.UnitTests/Common/Exceptions/CustomValidationExceptionTests.cs
@@ -0,0 +1,34 @@
+﻿using CoinJarGK.Application.Common.Exceptions;
+using FluentAssertions;
+using FluentValidation.Results;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace CoinJarGK.Application.UnitTests.Common.Exceptions
+{
+    public class  CustomValidationExceptionTests
+    {
+        [Test]
+        public void DefaultConstructorCreatesAnEmptyErrorDictionary()
+        {
+            var actual = new CustomValidationException().Errors;
+
+            actual.Keys.Should().BeEquivalentTo(Array.Empty<string>());
+        }
+
+        [Test]
+        public void SingleValidationFailureCreatesASingleElementErrorDictionary()
+        {
+            var failures = new List<ValidationFailure>
+            {
+                new ValidationFailure("Amount", "must be greater than 0"),
+            };
+
+            var actual = new CustomValidationException(failures).Errors;
+
+            actual.Keys.Should().BeEquivalentTo(new string[] { "Amount" });
+            actual["Amount"].Should().BeEquivalentTo(new string[] { "must be greater than 0" });
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Common/Exceptions/VolumeExceedExceptionTests.cs
@@ -0,0 +1,18 @@
+﻿using CoinJarGK.Application.Common.Exceptions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CoinJarGK.Application.UnitTests.Common.Exceptions
+{
+    public class VolumeExceedExceptionTests
+    {
+        [Test]
+        public void ShouldCreateSingleElementDetailDictionary() 
+        {
+            var actual = new CoinJarVolumeExceedException(42).Details;
+
+            actual.Count.Should().Be(1);
+            actual.Keys.Should().BeEquivalentTo(new string[] { "Volume" });
+        }
+    }
+}
