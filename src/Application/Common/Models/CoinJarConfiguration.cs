ad4352094a481b690aa18f741b9539888ae504be
William Madgwick <WMadgwick@netrec.co.za>
2021-11-07 18:54:57 +0200

    refactor: removed unused code

--- src/Application/Common/Models/CoinJarConfiguration.cs
+++ src/Application/Common/Models/CoinJarConfiguration.cs
@@ -1,17 +1,7 @@
-﻿using CoinJarGK.Application.Common.Enums;
-using System.Collections.Generic;
-
-namespace CoinJarGK.Application.Common.Models
+﻿namespace CoinJarGK.Application.Common.Models
 {
     public class CoinJarConfiguration
     {
         public decimal MaxVolume { get; set; }
-        public IList<USCoinConfiguration> USCoins { get; set; }
-    }
-
-    public class USCoinConfiguration
-    {
-        public USCoinDenomination Denomination { get; set; }
-        public decimal Volume { get; set; }
     }
 }
